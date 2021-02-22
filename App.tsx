f26c72c64e348d6ab5af009df98424b0c2247a8a
wahyu setiawan <cahyonowahyus123@gmail.com>
2021-02-22 19:31:47 +0700

    ppp

--- App.tsx
+++ App.tsx
@@ -4,9 +4,9 @@ import { SafeAreaView, Text } from "react-native";
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Hello world</Text>
+      <Text>Hello world ci cd</Text>
     </SafeAreaView>
   );
-}
+};
 
 export default App;
